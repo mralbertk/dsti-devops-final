551ed5789700e17077d95a451fc5ebf55ffcb9fe
mralberk <mralbertkonrad@gmail.com>
2022-05-16 21:59:47 +0200

    ci: added automatic api testing

--- userapi/conf/default.py
+++ userapi/conf/default.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 env_ip = os.environ.get("REDIS_IP")
-env_port = int(os.environ.get("REDIS_PORT"))
+env_port = os.environ.get("REDIS_PORT")
+env_port = int(env_port) if env_port else None
 
 redis_ip = env_ip if env_ip else "127.0.0.1"
 redis_port = env_port if env_port else 6379
